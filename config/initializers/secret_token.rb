492ad98eaa80d432c0300369abe4d89986244ff3
MakerSquare Student <students@themakersquare.com>
2013-07-29 16:06:03 -0500

    Inital cmommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readit3::Application.config.secret_token = '5ed8ff196642a5b027ccc585f3578bc3a239f94b60c872de9cd36433f8349a8c3d2ba2dbfdfd21c940d7bc318c36570ea48d5f5d4e1ec194f46c72e361fb119f'
