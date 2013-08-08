c7052502f05ff9831840dd28ddff9436859d10fc
Rachel <rachel.loziuk@gmail.com>
2013-08-08 14:37:31 -0500

    added has_many and belongs_to associations to user and links models

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
 class Link < ActiveRecord::Base
+  belongs_to :user
   attr_accessible :title, :url, :user_id
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  
+  has_many :links
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :email, :username, :password, :password_confirmation
+
+
 end
